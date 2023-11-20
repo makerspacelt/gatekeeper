e7a9a29074a06346f4e18087ef22a724000b67c3
darius <dariuskt@gmail.com>
2023-11-21 01:44:03 +0200

    adding healthcheck and self-healing

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 Schematics and PCB: https://github.com/makerspacelt/maglock-hw
 
 Dependencies: `apt install i2c-tools imagemagick nodejs bind9-dnsutils`.
@@ -6,6 +5,8 @@ Tested with Debian Bookworm.
 
 Run:
 * Create `config.js` based on `config-example.js`
-* Run `node main.js` (for dev)
+* Run `node main.js` (for dev) or `./start.sh` (with supervision)
 
-
+To make it run on startup:
+* Add `/root/gatekeeper-node/start.sh | logger -t MAGLOCK &` to /etc/rc.local
+* Make sure `rc-local.service` is enabled: `systemctl enable rc-local.service`
---
+++ start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" || true
+node main.js &
+echo $! > /run/gatekeeper.pid
+
+goReboot()
+{
+	for i in {1..20}; do
+		echo 1 >/sys/class/gpio/gpio6/value
+		sleep 0.2
+		echo 0 >/sys/class/gpio/gpio6/value
+		sleep 0.2
+	done
+	reboot
+}
+
+checkHealth()
+{
+	! ps "$(cat /run/gatekeeper.pid)" >/dev/null && echo "healthCheck failed: main process not found" && return 10
+
+	dmesg | grep -q "U[S]B disconnect"           && echo "healthCheck failed: USB disconnect" && return 20
+
+	dmesg | grep -q "disabled by hub (EMI[?])"   && echo "healthCheck failed: USB EMI" && return 30
+
+	dmesg | grep -q "Out of m[e]mory[:] Kill"    && echo "healthCheck failed: Out Of memory" && return 40
+
+	return 0
+}
+
+sleep 120 # wait before healthCheck
+
+while true; do
+	sleep 5
+	checkHealth || break
+done
+
+goReboot
+
